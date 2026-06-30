1f05c1d740824a44f593809615803de6618fcfd8
Ollie Sanders <osharpsand@gmail.com>
2026-06-30 13:35:59 -0600

    Fix Merge Error

--- setup.js
+++ setup.js
@@ -146,11 +146,6 @@ if (await promptYesOrNo('Would You Like To Generate A printshop.service File For
     execSync('sudo systemctl enable printshop-app', { stdio: 'inherit' });
 }
 
-if (production) {
-    console.log('Changing git Branch To Production...');
-    execSync('git checkout -b production origin/production');
-}
-
 if (production || await promptYesOrNo('Would You Like To Delete The Setup Helper?')) {
     fs.unlinkSync(path.resolve(__filename));
     fs.rmSync(setupFolder, { recursive: true });
@@ -158,6 +153,11 @@ if (production || await promptYesOrNo('Would You Like To Delete The Setup Helper
     let package = JSON.parse(fs.readFileSync(packageFile));
     delete package.scripts.setup;
     fs.writeFileSync(packageFile, JSON.stringify(package, null, 2));
+}
+
+if (production) {
+    console.log('Changing git Branch To Production...');
+    execSync('git checkout -b production origin/production');
 }
 
 if (!fs.existsSync(ordersFolder)) { fs.mkdirSync(ordersFolder); }
