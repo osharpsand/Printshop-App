f5361dae294ef3f4adca681bcbce6e7b82c2a69f
Ollie Sanders <osharpsand@gmail.com>
2026-06-30 14:21:59 -0600

    Fix start.sh Not Loading Node

--- .setup/start.sh
+++ .setup/start.sh
@@ -3,8 +3,22 @@ set -e
 
 cd "$(dirname "$0")"
 
+HOME_DIR="$(getent passwd "$(id -un)" | cut -d: -f6)"
+
+export NVM_DIR="$HOME_DIR/.nvm"
+
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    . "$NVM_DIR/nvm.sh"
+else
+    echo "Error: NVM not found at $NVM_DIR"
+    exit 1
+fi
+
+nvm use default >/dev/null
+
 if [ -d .setup ]; then
     echo "ERROR: Setup Has Not Been Completed. Please Run pnpm run setup First."
+    exit 1
 fi
 
 echo "Pulling Latest Changes..."
